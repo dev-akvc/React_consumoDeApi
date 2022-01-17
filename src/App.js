1967833a2d4c90810c15004663f205e759dc693e
Anna Karolyne <dev.akvc@gmail.com>
2022-01-17 10:22:33 -0300

    feat: Criar componente App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react, {Component} from "react";
+import Login from "./components/Header/pages";
+
+export default class App extends Component {
+    render() {
+        return(
+            <div className="App">
+                <Login/>
+            </div>
+        );
+    }
+}
